b250b4057aa7d717870ed061222c4f727c544d91
JadenMatt <jadenmatthias1@gmail.com>
2025-08-20 19:20:18 +0100

    Create step5_suspects.sql

---
+++ queries/step5_suspects.sql
@@ -0,0 +1,11 @@
+select *
+from get_fit_now_check_in
+where check_in_date = 20180109;
+
+select *
+from get_fit_now_member
+where id = '48Z55' or id = '48Z7A';
+
+select *
+from person
+where id = '67318' OR id = '28819';
